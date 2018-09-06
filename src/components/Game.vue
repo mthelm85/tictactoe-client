251e83b70decb5936c8d2b6c9772121b34eeb98b
Matt Helm <mthelm85@gmail.com>
2018-09-06 18:57:42 -0500

    added functions to call api

--- src/components/Game.vue
+++ src/components/Game.vue
@@ -3,36 +3,45 @@
     <div class="row d-flex justify-content-center">
       <div class="col" style="max-width: 300px;">
         <div class="row mt-3">
-          <div class="col-4 square sq0 d-flex justify-content-center align-items-center display-4">
-            X
+          <div @click="userMove(0)" class="col-4 square sq0 d-flex justify-content-center align-items-center display-4">
+            <span v-show="squares[0].X">X</span>
+            <span v-show="squares[0].O">O</span>
           </div>
-          <div class="col-4 square sq1 d-flex justify-content-center align-items-center display-4">
-            O
+          <div @click="userMove(1)" class="col-4 square sq1 d-flex justify-content-center align-items-center display-4">
+            <span v-show="squares[1].X">X</span>
+            <span v-show="squares[1].O">O</span>
           </div>
-          <div class="col-4 square sq2 d-flex justify-content-center align-items-center display-4">
-            X
-          </div>
-        </div>
-        <div class="row">
-          <div class="col-4 square sq3 d-flex justify-content-center align-items-center display-4">
-          O
-          </div>
-          <div class="col-4 square sq4 d-flex justify-content-center align-items-center display-4">
-            X
-          </div>
-          <div class="col-4 square sq5 d-flex justify-content-center align-items-center display-4">
-            O
+          <div @click="userMove(2)" class="col-4 square sq2 d-flex justify-content-center align-items-center display-4">
+            <span v-show="squares[2].X">X</span>
+            <span v-show="squares[2].O">O</span>
           </div>
         </div>
         <div class="row">
-          <div class="col-4 square sq6 d-flex justify-content-center align-items-center display-4">
-            X
+          <div @click="userMove(3)" class="col-4 square sq3 d-flex justify-content-center align-items-center display-4">
+            <span v-show="squares[3].X">X</span>
+            <span v-show="squares[3].O">O</span>
           </div>
-          <div class="col-4 square sq7 d-flex justify-content-center align-items-center display-4">
-            O
+          <div @click="userMove(4)" class="col-4 square sq4 d-flex justify-content-center align-items-center display-4">
+            <span v-show="squares[4].X">X</span>
+            <span v-show="squares[4].O">O</span>
           </div>
-          <div class="col-4 square sq8 d-flex justify-content-center align-items-center display-4">
-            X
+          <div @click="userMove(5)" class="col-4 square sq5 d-flex justify-content-center align-items-center display-4">
+            <span v-show="squares[5].X">X</span>
+            <span v-show="squares[5].O">O</span>
+          </div>
+        </div>
+        <div class="row">
+          <div @click="userMove(6)" class="col-4 square sq6 d-flex justify-content-center align-items-center display-4">
+            <span v-show="squares[6].X">X</span>
+            <span v-show="squares[6].O">O</span>
+          </div>
+          <div @click="userMove(7)" class="col-4 square sq7 d-flex justify-content-center align-items-center display-4">
+            <span v-show="squares[7].X">X</span>
+            <span v-show="squares[7].O">O</span>
+          </div>
+          <div @click="userMove(8)" class="col-4 square sq8 d-flex justify-content-center align-items-center display-4">
+            <span v-show="squares[8].X">X</span>
+            <span v-show="squares[8].O">O</span>
           </div>
         </div>
       </div>
@@ -41,7 +50,80 @@
 </template>
 
 <script>
+import axios from 'axios'
 export default {
+  data () {
+    return {
+      probabilities: [],
+      state: [.5, .5, .5, .5, .5, .5, .5, .5, .5],
+      squares: [
+          {
+            X: false,
+            O: false
+          },
+          {
+            X: false,
+            O: false
+          },
+          {
+            X: false,
+            O: false
+          },
+          {
+            X: false,
+            O: false
+          },
+          {
+            X: false,
+            O: false
+          },
+          {
+            X: false,
+            O: false
+          },
+          {
+            X: false,
+            O: false
+          },
+          {
+            X: false,
+            O: false
+          },
+          {
+            X: false,
+            O: false
+          }
+      ]
+    }
+  },
+
+  created () {
+    this.nextMove()
+  },
+
+  methods: {
+    async nextMove () {
+      this.probabilities.length = 0
+      let array = this.state.slice()
+      for (let i = 0; i < 9; i++) {
+        array.splice(i, 1, 1)
+        let res = await axios.post('http://localhost:3000/get-probability', {
+          array: array
+        })
+        this.probabilities.push(res.data.Result[0])
+        array = this.state.slice()
+      }
+      let highestProb = Math.max(...this.probabilities)
+      let index = this.probabilities.indexOf(highestProb)
+      this.squares[index].X = true
+      this.state.splice(index, 1, 1)
+    },
+    userMove (n) {
+      this.squares[n].O = true
+      this.state.splice(n, 1, 0)
+      this.nextMove()
+    }
+  }
 }
 </script>
 
@@ -51,7 +133,7 @@ export default {
   height: 100px;
 }
 .square:hover {
-  background-color: gray;
+  background-color: #ccf4ff;
   cursor: pointer;
 }
 .sq0, .sq1, .sq3, .sq4 {
